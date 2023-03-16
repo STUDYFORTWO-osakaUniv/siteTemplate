ce855fa89c05844894faac2ac355ed1302b02fea
STUDYFORTWO-osakaUniv <127311217+STUDYFORTWO-osakaUniv@users.noreply.github.com>
2023-03-16 22:23:42 +0900

    230316_2223

--- js/postInfo.js
+++ js/postInfo.js
@@ -1,5 +1,5 @@
 async function postInfo(data) {
-    const repoName = location.pathname; // "/"除く
+    const repoName = location.pathname.replace(/\//g, ""); // "/"除く
     data = {
         ...data,
         repoName: repoName
@@ -7,7 +7,7 @@ async function postInfo(data) {
     let url;
     console.log("post:", data);
 
-    if(data.type === "reserve"){ url = "https://script.google.com/macros/s/AKfycbykLnW45WGwUJgJu_XAONK7t6hR9v2C93jEdeT37xbjHCupB-UMcEypdkzVf4AgGqmO/exec"}
+    if (data.type === "reserve") { url = "https://script.google.com/macros/s/AKfycbykLnW45WGwUJgJu_XAONK7t6hR9v2C93jEdeT37xbjHCupB-UMcEypdkzVf4AgGqmO/exec" }
 
     // Promise
     const response = await fetch(url,
